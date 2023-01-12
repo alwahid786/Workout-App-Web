92f96d2d2734a6ca5b35506aaba337ccdcea762e
Wahid Ahmad <wahid.a@kodextech.com>
2023-01-12 16:04:44 +0500

    updated toaster

--- resources/views/includes/website/footer.blade.php
+++ resources/views/includes/website/footer.blade.php
@@ -3,6 +3,7 @@
  <script src="{{ asset('public/assets/js/bootstrap.min.js') }}"></script>
  <script src="{{ asset('public/assets/js/bootstrap.js') }}"></script>
  <script src="{{ asset('public/assets/js/bootstrap.bundle.min.js') }}"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js" integrity="sha512-lbwH47l/tPXJYG9AcFNoJaTMhGvYWhVM9YI43CT+uteTRRaiLCui8snIgyAN8XWgNjNhCqlAUdzZptso6OCoFQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
  <!-- {{-- AOS --}}
  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
  <script>
