7cee99cc5fabaea176710bbe24a9749dc8b05b63
zeeshankhan001 <zeeshan.h@kodextech.com>
2023-01-31 09:53:06 +0500

    updated

--- resources/views/layouts/trainerSide/default.blade.php
+++ resources/views/layouts/trainerSide/default.blade.php
@@ -7,10 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>WORKITPT</title>
-
-
     @include('includes.trainerSide.header')
-
 </head>
 
 <body>
--- resources/views/pages/trainerSide/auth/forgetpassword.blade.php
+++ resources/views/pages/trainerSide/auth/forgetpassword.blade.php
@@ -30,5 +30,4 @@
 </div>
 @endsection
 @section('insertsfooter')
-
 @endsection
