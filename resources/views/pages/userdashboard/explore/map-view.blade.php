e12dc3f7b6fdceeb4468d8c7d5bfacebf52c713b
Wahid Ahmad <wahid.a@kodextech.com>
2023-02-01 17:41:39 +0500

    updated ui issue

--- resources/views/pages/userdashboard/explore/map-view.blade.php
+++ resources/views/pages/userdashboard/explore/map-view.blade.php
@@ -256,14 +256,14 @@
         </div>
         <div class="filter-section py-4 px-2">
             <div class="row">
-                <div class="col-12 my-auto">
+                <!-- <div class="col-12 my-auto">
                     <div class="filter-menu-left my-auto px-sm-2">
                         <div class="filter-left-heading">
                             <h1>Sessions</h1>
                         </div>
 
                     </div>
-                </div>
+                </div> -->
                 <div class="col-xl-6">
                     <div class=" filter-menu">
                         <div class=" filter-menu-right">
