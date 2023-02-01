abc75f3e07d71f01bec7724a14333d30070a07b1
zeeshankhan001 <zeeshan.h@kodextech.com>
2023-02-01 22:24:10 +0500

    updated

--- resources/views/pages/userdashboard/dashboard/user-dashboard.blade.php
+++ resources/views/pages/userdashboard/dashboard/user-dashboard.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <link rel="stylesheet" href="{{asset('public/assets/css/calendar.css')}}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/air-datepicker/2.2.3/css/datepicker.css">
-<link rel="stylesheet" href="./style.css">
+<!-- <link rel="stylesheet" href="./style.css"> -->
 <style>
     .workout-banner-card {
         background: #FDD9CE;
@@ -972,7 +972,6 @@
 @endsection
 @section('insertsfooter')
 <script src="{{ asset('public/assets/AS/js/mobiscroll.jquery.min.js') }}"></script>
-
 <script>
     mobiscroll.setOptions({
         locale: mobiscroll.localeEn, // Specify language like: locale: mobiscroll.localePl or omit setting to use default
@@ -1018,7 +1017,7 @@
         });
     });
 </script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.js"></script>
