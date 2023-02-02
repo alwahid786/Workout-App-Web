0576be2da6c3f26f0e60a7830456d1aeb14ef1aa
zeeshankhan001 <zeeshan.h@kodextech.com>
2023-02-02 23:49:27 +0500

    updated

--- resources/views/pages/trainerSide/account-stepfour.blade.php
+++ resources/views/pages/trainerSide/account-stepfour.blade.php
@@ -1,6 +1,160 @@
 @extends('layouts.trainerSide.default')
-<link rel="stylesheet" href="{{asset('public/assets/trainercss/stepfour.css')}}">
-<link rel="stylesheet" href="{{asset('public/assets/trainercss/pagination.css')}}">
+<!-- <link rel="stylesheet" href="{{asset('public/assets/trainercss/stepfour.css')}}">
+<link rel="stylesheet" href="{{asset('public/assets/trainercss/pagination.css')}}"> -->
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+
+
+    /* new */
+    .stripe-container {}
+
+    .stripe-wrapper h1 {
+        font-size: 2.5rem;
+        font-weight: 900;
+        color: #6772E5;
+    }
+
+    .stripe-wrapper p {
+        font-size: 1.5rem;
+        color: black;
+        font-weight: 500;
+    }
+
+    .stripe-wrapper a {
+        text-decoration: none !important;
+        box-shadow: none !important;
+        color: #E37048;
+        background: white;
+        border: 1px solid #E37048;
+        width: 180px;
+        border-radius: 5px;
+        height: 40px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 0 auto;
+    }
+
+    .stripe-wrapper a:hover {
+        background: #E37048;
+        color: white;
+    }
+
+    .stripe-wrapper img {
+        max-width: 200px;
+        width: 100%;
+    }
+
+    .stripe-container {
+        background-image: url('../../../../workitpt_web/public/assets/trainerimages/stripe-bg.svg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        padding-top: 15rem;
+        padding-bottom: 15rem;
+        position: relative;
+    }
+
+    .stripe-wrapper {
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    /* p */
+    .pagination-wrapper {
+        background-color: #F8F8F8;
+        border-radius: 10px;
+        position: relative;
+        padding-top: 1rem;
+        padding-bottom: 3rem;
+        padding-left: 0.5rem;
+        padding-right: 0.5rem;
+    }
+
+    .pagination-border {
+        border: 1px solid #E37048;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 100%;
+        z-index: 1;
+    }
+
+    .pagination-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: relative;
+    }
+
+    .pagination-box {
+        position: relative;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        background: #f5bfac;
+        border: 2px solid #E37048;
+        z-index: 2;
+    }
+
+    .active-pagination {
+        background: #E37048;
+    }
+
+    .pagination-box h1 {
+        position: absolute;
+        color: white;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 1rem;
+        font-weight: 700;
+        margin-bottom: 0px;
+    }
+
+    .pagination-box p {
+        font-size: 0.6rem;
+        margin-bottom: 0px;
+        color: black;
+        position: absolute;
+        top: 55px;
+        left: 50%;
+        text-align: center;
+        transform: translate(-50%, -50%);
+
+    }
+
+    @media screen and (max-width:576px) {
+        .pagination-box {
+            position: relative;
+            width: 30px;
+            height: 30px;
+        }
+
+        .pagination-box h1 {
+            position: absolute;
+            color: white;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 0.8rem;
+            font-weight: 700;
+            margin-bottom: 0px;
+        }
+
+        .pagination-box p {
+            font-size: 0.5rem;
+        }
+    }
+</style>
 @section('content')
 <!-- header-section -->
 <div class="container-fluid update-info-header">
