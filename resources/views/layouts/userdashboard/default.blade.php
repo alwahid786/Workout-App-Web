600dd7d874500baaa3dcd65643ea854cab09ee3a
Wahid Ahmad <wahid.a@kodextech.com>
2023-04-11 15:06:06 +0500

    update favicon

--- resources/views/layouts/userdashboard/default.blade.php
+++ resources/views/layouts/userdashboard/default.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="{{asset('public/assets/images/dumbbell.png')}}">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>WORKITPT</title>
 
